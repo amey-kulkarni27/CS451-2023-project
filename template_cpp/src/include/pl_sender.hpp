274f366a853ff74520f2fd9100a28d8868fa6556
amey-kulkarni27 <amey2704@gmail.com>
2023-11-01 14:27:58 +0100

    perfect link sender sent

--- template_cpp/src/include/pl_sender.hpp
+++ template_cpp/src/include/pl_sender.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <iostream>
+#include <set>
 
 #include <parser.hpp>
-// #include <stubborn_links.hpp>
+#include <stubborn_sender.hpp>
 
 class PLSender{
 	
@@ -15,6 +16,10 @@ public:
 	pp2pSend(std::string msg){
 		// append id to the start of the message so that the receiver knows
 		// get stubborn links to infinitely send that message
+		unsigned long ts = id;
+		msg = std::to_string(ts) + "_" + msg;
+		StubbornSender::sp2pSend(ts, msg);
+		id++;
 	}
 
 
@@ -22,10 +27,17 @@ private:
 	Parser::Host receiver;
 	Parser::Host self;
 	unsigned long id = 0;
+	set<unsigned long> acked; // those messages that we have received an ACK for
 
 	pp2pReceive(std::string ackMsg){
 		// get the ID of the message
 		// get stubborn links to stop sending that message
+		std::string ts_str = ackMsg.substr(0, ackMsg.find('_'));
+		unsigned long ts = std::stoul(ts_str);
+		std::pair<std::set<int>::iterator,bool> ret; // store return value of insert
+		ret = ackMsg.insert(ts);
+		if(ret -> second)
+			StubbornSender::stop(ts);
 	}
 
 }
